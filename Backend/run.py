53b6848d1b884f6a19957f35e4c41f6047ecec1a
shrehanrajsingh <shrehanofficial@gmail.com>
2025-03-27 00:39:32 +0530

    report changes

--- Backend/run.py
+++ Backend/run.py
@@ -1,6 +1,8 @@
 import uvicorn
 from app.config import settings
+from app.report.report import *
 
 if __name__ == "__main__":
     print(f"Starting server at http://{settings.HOST}:{settings.PORT}")
+    report_test1()
     uvicorn.run("app.main:app", host=settings.HOST, port=settings.PORT, reload=True)
